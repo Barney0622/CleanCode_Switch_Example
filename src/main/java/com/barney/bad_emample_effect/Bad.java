d156fb16695eb7695b6206f6037dcc48e004c7bc
Barney <nn637159@gmail.com>
2023-10-30 18:35:41 +0800

    新增: Bad Example Effect

---
+++ src/main/java/com/barney/bad_emample_effect/Bad.java
@@ -0,0 +1,54 @@
+package com.barney.bad_emample_effect;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Bad {
+
+    public BigDecimal calculateMoney(Employee employee) {
+        switch (employee.getEmployeeType()) {
+            case COMMISSIONE:
+                return BigDecimal.valueOf(10000);
+            case SALARIED:
+                return BigDecimal.valueOf(1000);
+            case HOURLY:
+                return BigDecimal.valueOf(10);
+            case NEW_EMPLOYEE:
+                return BigDecimal.valueOf(40);
+            default:
+                return BigDecimal.ZERO;
+        }
+    }
+
+    public boolean isPayDay(Employee employee) {
+        LocalDate today = LocalDate.now();
+
+        switch (employee.getEmployeeType()) {
+            case COMMISSIONE:
+                return today.getDayOfMonth() == 5;
+            case SALARIED:
+                return today.getDayOfMonth() == 10;
+            case HOURLY:
+                return today.getDayOfMonth() == 25;
+            case NEW_EMPLOYEE:
+                return today.getDayOfMonth() == 1;
+            default:
+                return false;
+        }
+    }
+
+    public void deliverPay(Employee employee) {
+        switch (employee.getEmployeeType()) {
+            case COMMISSIONE:
+                System.out.println("支票");
+            case SALARIED:
+                System.out.println("轉帳");
+            case HOURLY:
+                System.out.println("現金");
+            case NEW_EMPLOYEE:
+                System.out.println("楓幣");
+            default:
+                System.out.println("Barney的戶頭");
+        }
+    }
+}
---
+++ src/main/java/com/barney/bad_emample_effect/Employee.java
@@ -0,0 +1,16 @@
+package com.barney.bad_emample_effect;
+
+import com.barney.pojo.EmployeeType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private String name;
+    private EmployeeType employeeType;
+}
